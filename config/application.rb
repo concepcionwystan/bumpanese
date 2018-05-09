e820b8593dc6863a56aac9718af4fd4000f9637a
Wystan Concepcion <concepcionwystan@gmail.com>
2018-05-10 01:15:02 +0800

    Removed cors gem

--- config/application.rb
+++ config/application.rb
@@ -28,12 +28,12 @@ module Bumpanese
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins '*'
+    #     resource '*', :headers => :any, :methods => [:get, :post, :options]
+    #   end
+    # end
     config.api_only = true
     # config.action_dispatch.default_headers = {
     #     'Access-Control-Allow-Origin' => '*',
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   resources :products
   root 'products#index'
+  namespace :api do
+    #
+    # Other routes
+    # 
+    match '*path', :controller => '/api', :action => 'handle_options_request', :via => [:get, :post, :options]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
